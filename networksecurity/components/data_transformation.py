647b1fa63ffdb33f3a86e1a3b35f151e0408e005
ranjitdarji <ranjitdarji07@gmail.com>
2026-07-07 18:14:16 +0530

    pipeline is completed

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -105,6 +105,9 @@ class DataTransformation:
                 preprocessor_object
             )
 
+
+            save_object("final_model/preprocessor.pkl",preprocessor_object,)
+
             
             data_transformation_artifact = DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
